723d6db16a9728f0febbae20608466ada8fa19d4
Torsten Oltmanns <Torsten.O@gmx.net>
2023-11-23 10:00:11 +0800

    refactored package names to differenciate between BMS and inverter

---
+++ bms-daly-can/src/main/java/com/airepublic/bmstoinverter/bms/daly/can/DalyBmsCANProcessor.java
@@ -0,0 +1,125 @@
+package com.airepublic.bmstoinverter.bms.daly.can;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.HexFormat;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.airepublic.bmstoinverter.bms.daly.common.AbstractDalyBmsProcessor;
+import com.airepublic.bmstoinverter.bms.daly.common.DalyCommand;
+import com.airepublic.bmstoinverter.bms.daly.common.DalyMessage;
+import com.airepublic.bmstoinverter.core.Port;
+import com.airepublic.bmstoinverter.core.PortType;
+import com.airepublic.bmstoinverter.core.Protocol;
+import com.airepublic.bmstoinverter.core.bms.data.EnergyStorage;
+import com.airepublic.bmstoinverter.core.protocol.can.CANPort;
+
+import jakarta.inject.Inject;
+
+/**
+ * The class to handle CAN messages from a Daly BMS.
+ */
+@PortType(Protocol.CAN)
+public class DalyBmsCANProcessor extends AbstractDalyBmsProcessor {
+    private final static Logger LOG = LoggerFactory.getLogger(DalyBmsCANProcessor.class);
+    private final ByteBuffer sendFrame = ByteBuffer.allocateDirect(16).order(ByteOrder.LITTLE_ENDIAN);
+    @Inject
+    private EnergyStorage energyStorage;
+
+    @Override
+    protected List<ByteBuffer> sendMessage(final int bmsNo, final DalyCommand cmd, final byte[] data) throws IOException {
+        final ByteBuffer sendFrame = prepareSendFrame(bmsNo + 1, cmd, data);
+        int framesToBeReceived = getResponseFrameCount(cmd);
+        final int frameCount = framesToBeReceived;
+        int skip = 20;
+        final List<ByteBuffer> readBuffers = new ArrayList<>();
+        final Port port = energyStorage.getBatteryPack(bmsNo).port;
+
+        LOG.debug("SEND: {}", Port.printBuffer(sendFrame));
+        ((CANPort) port).sendExtendedFrame(sendFrame);
+
+        // read frames until the requested frame is read
+        do {
+            skip--;
+
+            for (int i = 0; i < frameCount; i++) {
+                final ByteBuffer receiveFrame = port.receiveFrame(t -> true);
+
+                LOG.debug("RECEIVED: {}", Port.printBuffer(receiveFrame));
+
+                final byte receiver = (byte) (receiveFrame.getInt(0) >> 8 & 0x000000FF);
+                final byte command = (byte) (receiveFrame.getInt(0) >> 16 & 0x000000FF);
+
+                if (receiver == (byte) 0x40 && command == (byte) cmd.id) {
+                    readBuffers.add(receiveFrame);
+                    framesToBeReceived--;
+                }
+
+                getMessageHandler().handleMessage(convertReceiveFrameToDalyMessage(receiveFrame));
+            }
+        } while (framesToBeReceived > 0 & skip > 0);
+
+        LOG.debug("Command 0x{} to BMS {} successfully sent and received!", HexFormat.of().toHexDigits(cmd.id), bmsNo + 1);
+        return readBuffers;
+    }
+
+
+    @Override
+    protected ByteBuffer prepareSendFrame(final int address, final DalyCommand cmd, final byte[] data) {
+        sendFrame.rewind();
+
+        // frame id
+        long frameId = 0L;
+        frameId = 0x18;
+        frameId = frameId << 8;
+        frameId += (byte) cmd.id;
+        frameId = frameId << 8;
+        frameId += (byte) address;
+        frameId = frameId << 8;
+        frameId += 0x40;
+        sendFrame.putInt((int) frameId);
+
+        // header
+        sendFrame.put((byte) 0x08) // data length
+                .put((byte) 0) // flags
+                .putShort((short) 0); // skip 2 bytes
+
+        // data
+        for (final byte element : data) {
+            sendFrame.put(element);
+        }
+
+        sendFrame.rewind();
+
+        return sendFrame;
+    }
+
+
+    @Override
+    public DalyMessage convertReceiveFrameToDalyMessage(final ByteBuffer buffer) {
+        buffer.rewind();
+
+        final DalyMessage msg = new DalyMessage();
+        final int frameId = buffer.getInt(0);
+        msg.address = (byte) (frameId & 0x000000FF);
+        msg.cmd = DalyCommand.valueOf(frameId >> 16 & 0x000000FF);
+
+        final byte[] dataBytes = new byte[buffer.get(4)];
+        buffer.get(8, dataBytes);
+        msg.data = ByteBuffer.wrap(dataBytes);
+
+        if (LOG.isDebugEnabled()) {
+            LOG.info("DALY Message: frameId= " + Integer.toHexString(frameId)
+                    + ", address=" + HexFormat.of().toHexDigits(msg.address)
+                    + ", dataId=" + HexFormat.of().toHexDigits(msg.cmd.id)
+                    + ", data=" + HexFormat.of().formatHex(dataBytes));
+        }
+
+        return msg;
+    }
+}
---
+++ bms-pylon-can/src/main/java/com/airepublic/bmstoinverter/bms/pylon/can/PylonBmsCANProcessor.java
@@ -0,0 +1,204 @@
+package com.airepublic.bmstoinverter.bms.pylon.can;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.airepublic.bmstoinverter.core.Bms;
+import com.airepublic.bmstoinverter.core.Port;
+import com.airepublic.bmstoinverter.core.PortType;
+import com.airepublic.bmstoinverter.core.Protocol;
+import com.airepublic.bmstoinverter.core.bms.data.EnergyStorage;
+import com.airepublic.bmstoinverter.core.protocol.can.CAN;
+
+import jakarta.inject.Inject;
+
+/**
+ * The class to handle {@link CAN} messages from a Pylon {@link Bms}.
+ */
+
+@PortType(Protocol.CAN)
+public class PylonBmsCANProcessor implements Bms {
+    private final static Logger LOG = LoggerFactory.getLogger(PylonBmsCANProcessor.class);
+    @Inject
+    private EnergyStorage energyStorage;
+
+    @Override
+    public void process() {
+        for (int bmsNo = 0; bmsNo < energyStorage.getBatteryPackCount(); bmsNo++) {
+            try {
+                final Port port = energyStorage.getBatteryPack(bmsNo).port;
+                final ByteBuffer frame = port.receiveFrame(null);
+                final int frameId = frame.getInt();
+                final byte[] bytes = new byte[8];
+                frame.get(bytes);
+                final ByteBuffer data = ByteBuffer.wrap(bytes);
+
+                switch (frameId) {
+                    case 0x351:
+                        readChargeDischargeInfo(bmsNo, data);
+                    break;
+                    case 0x355:
+                        readSOC(bmsNo, data);
+                    break;
+                    case 0x356:
+                        readBatteryVoltage(bmsNo, data);
+                    break;
+                    case 0x35C:
+                        requestChargeDischargeConfigChange(bmsNo, data);
+                    break;
+                    case 0x370:
+                        readMinMaxTemperatureVoltage(bmsNo, data);
+                    break;
+                    case 0x371:
+                        readTemperatureIds(bmsNo, data);
+                    break;
+                    case 0x35E:
+                        readManufacturer(bmsNo, data);
+                    break;
+                    case 0x359:
+                        readAlarms(bmsNo, data);
+                    break;
+                }
+
+            } catch (final IOException e) {
+                LOG.error("Error receiving frame!", e);
+            }
+        }
+    }
+
+
+    // 0x351
+    private void readChargeDischargeInfo(final int bmsNo, final ByteBuffer data) {
+        // Battery charge voltage (0.1V) - uint_16
+        energyStorage.getBatteryPack(bmsNo).maxPackVoltageLimit = data.getChar();
+        // Charge current limit (0.1A) - sint_16
+        energyStorage.getBatteryPack(bmsNo).maxPackChargeCurrent = data.getShort();
+        // Discharge current limit (0.1A) - sint_16
+        energyStorage.getBatteryPack(bmsNo).maxPackDischargeCurrent = data.getShort();
+        // Battery discharge voltage (0.1V) - uint_16
+        energyStorage.getBatteryPack(bmsNo).minPackVoltageLimit = data.getChar();
+
+    }
+
+
+    // 0x355
+    private void readSOC(final int bmsNo, final ByteBuffer data) {
+        // SOC (1%) - uint_16
+        energyStorage.getBatteryPack(bmsNo).maxPackDischargeCurrent = data.getChar();
+        // SOH (1%) - uint_16
+        energyStorage.getBatteryPack(bmsNo).packVoltage = data.getChar();
+    }
+
+
+    // 0x356
+    private void readBatteryVoltage(final int bmsNo, final ByteBuffer data) {
+        // Battery voltage (0.01V) - uint_16
+        energyStorage.getBatteryPack(bmsNo).packVoltage = data.getShort();
+        // Battery current (0.1A) - uint_16
+        energyStorage.getBatteryPack(bmsNo).packCurrent = data.getShort();
+        // Battery current (0.1C) - uint_16
+        energyStorage.getBatteryPack(bmsNo).tempAverage = data.getShort();
+    }
+
+
+    // 0x35C
+    private void requestChargeDischargeConfigChange(final int bmsNo, final ByteBuffer data) {
+        final byte bits = data.get();
+
+        if (bitRead(bits, 4)) {
+            // request force-charge II
+        }
+
+        if (bitRead(bits, 5)) {
+            // request force-charge I
+        }
+
+        if (bitRead(bits, 6)) {
+            // request discharge enable
+        }
+
+        if (bitRead(bits, 7)) {
+            // request charge enable
+        }
+    }
+
+
+    // 0x370
+    private void readMinMaxTemperatureVoltage(final int bmsNo, final ByteBuffer data) {
+        // Maximum cell temperature (0.1C) - uint_16
+        energyStorage.getBatteryPack(bmsNo).tempMax = data.getShort();
+        // Minimum cell temperature (0.1C) - uint_16
+        energyStorage.getBatteryPack(bmsNo).tempMin = data.getShort();
+        // Maximum cell voltage (0.1V) - uint_16
+        energyStorage.getBatteryPack(bmsNo).maxCellmV = data.getShort();
+        // Minimum cell voltage (0.1V) - uint_16
+        energyStorage.getBatteryPack(bmsNo).minCellmV = data.getShort();
+    }
+
+
+    // 0x371
+    private void readTemperatureIds(final int bmsNo, final ByteBuffer data) {
+        // Maximum cell temperature (0.1C) - uint_16
+        // energyStorage.getBatteryPack(bmsNo).tempMax = data.getShort();
+        // Minimum cell temperature (0.1C) - uint_16
+        // energyStorage.getBatteryPack(bmsNo).tempMin = data.getShort();
+        // Maximum cell voltage id - uint_16
+        energyStorage.getBatteryPack(bmsNo).maxCellVNum = data.getShort();
+        // Minimum cell voltage id - uint_16
+        energyStorage.getBatteryPack(bmsNo).minCellVNum = data.getShort();
+    }
+
+
+    // 0x35E
+    private void readManufacturer(final int bmsNo, final ByteBuffer data) {
+        final char first = (char) data.get();
+        final char second = (char) data.get();
+
+        energyStorage.getBatteryPack(bmsNo).manufacturerCode = "" + first + second;
+    }
+
+
+    // 0x359
+    private void readAlarms(final int bmsNo, final ByteBuffer data) {
+        // read first 8 bits
+        int value = data.getInt();
+
+        // protection alarms
+        energyStorage.getBatteryPack(bmsNo).alarms.levelTwoCellVoltageTooHigh.value = bitRead(value, 1);
+        energyStorage.getBatteryPack(bmsNo).alarms.levelTwoCellVoltageTooLow.value = bitRead(value, 2);
+        energyStorage.getBatteryPack(bmsNo).alarms.levelTwoDischargeTempTooHigh.value = bitRead(value, 3);
+        energyStorage.getBatteryPack(bmsNo).alarms.levelTwoDischargeTempTooLow.value = bitRead(value, 4);
+        energyStorage.getBatteryPack(bmsNo).alarms.levelTwoDischargeCurrentTooHigh.value = bitRead(value, 7);
+        energyStorage.getBatteryPack(bmsNo).alarms.levelTwoChargeCurrentTooHigh.value = bitRead(value, 8);
+
+        // warning alarms
+        energyStorage.getBatteryPack(bmsNo).alarms.levelOneCellVoltageTooHigh.value = bitRead(value, 17);
+        energyStorage.getBatteryPack(bmsNo).alarms.levelOneCellVoltageTooLow.value = bitRead(value, 18);
+        energyStorage.getBatteryPack(bmsNo).alarms.levelOneChargeTempTooHigh.value = bitRead(value, 19);
+        energyStorage.getBatteryPack(bmsNo).alarms.levelOneChargeTempTooLow.value = bitRead(value, 20);
+        energyStorage.getBatteryPack(bmsNo).alarms.levelOneDischargeCurrentTooHigh.value = bitRead(value, 23);
+        energyStorage.getBatteryPack(bmsNo).alarms.levelOneChargeCurrentTooHigh.value = bitRead(value, 24);
+        energyStorage.getBatteryPack(bmsNo).alarms.failureOfIntranetCommunicationModule.value = bitRead(value, 27);
+        energyStorage.getBatteryPack(bmsNo).alarms.levelTwoCellVoltageDifferenceTooHigh.value = bitRead(value, 28);
+
+        energyStorage.getBatteryPack(bmsNo).numberOfCells = data.get();
+
+        // skip two bytes
+        data.getShort();
+
+        // dip switch
+        value = data.get();
+        final int packNo = value >> 4;
+        final int cellNo = value & 0x0F;
+
+    }
+
+
+    private boolean bitRead(final int value, final int index) {
+        return (value >> index & 1) == 1;
+    }
+
+}
