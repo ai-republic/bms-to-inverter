cd099091c0fad54277745d5b843cf03a2c4a23da
Torsten Oltmanns <Torsten.O@gmx.net>
2024-01-18 10:00:56 +0800

    added beans.xml to other inverter projects

--- core-api/src/main/java/com/airepublic/bmstoinverter/core/protocol/modbus/ModBus.java
+++ core-api/src/main/java/com/airepublic/bmstoinverter/core/protocol/modbus/ModBus.java
@@ -3,7 +3,9 @@ package com.airepublic.bmstoinverter.core.protocol.modbus;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
 
 import jakarta.inject.Qualifier;
 
@@ -13,6 +15,7 @@ import jakarta.inject.Qualifier;
 @Qualifier
 @Retention(RUNTIME)
 @Documented
+@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.TYPE })
 public @interface ModBus {
 
 }
