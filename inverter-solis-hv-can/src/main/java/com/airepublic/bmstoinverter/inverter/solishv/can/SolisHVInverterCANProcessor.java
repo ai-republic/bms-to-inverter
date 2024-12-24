7a5e536953b33ff48aa04a88d4336070b4b1f484
Dalibor Majksner <62405450+DMIINC@users.noreply.github.com>
2024-12-24 12:17:00 -0500

    Update SolisHVInverterCANProcessor.java

--- inverter-solis-hv-can/src/main/java/com/airepublic/bmstoinverter/inverter/solishv/can/SolisHVInverterCANProcessor.java
+++ inverter-solis-hv-can/src/main/java/com/airepublic/bmstoinverter/inverter/solishv/can/SolisHVInverterCANProcessor.java
@@ -136,9 +136,9 @@ public class SolisHVInverterCANProcessor extends Inverter {
         // Battery voltage (0.1V)
         frame.putShort((short) pack.packVoltage);
         // Battery current (0.1A) offset -3000A
-        frame.putShort((short) (pack.packCurrent + 30000));
+        frame.putShort((short) (pack.packCurrent - 3000));
         // second level temperature (0.1 Celcius) offset -100C
-        frame.putShort((short) (pack.tempAverage + 1000));
+        frame.putShort((short) (pack.tempAverage - 100));
         // Battery SOC (1%)
         frame.put((byte) (pack.packSOC / 10));
         // Battery SOH (1%)
@@ -160,9 +160,9 @@ public class SolisHVInverterCANProcessor extends Inverter {
 
         // TODO check if these should be swapped as described in Growatt_Battery_BMS.pdf
         // Max charge current (0.1A) offset -3000A
-        frame.putShort((short) (pack.maxPackChargeCurrent + 30000));
+        frame.putShort((short) (pack.maxPackChargeCurrent - 3000));
         // Max discharge current (0.1A) offset -3000A
-        frame.putShort((short) (pack.maxPackDischargeCurrent + 30000));
+        frame.putShort((short) (pack.maxPackDischargeCurrent - 3000));
 
         LOG.debug("Sending max/min voltage, current, charge and discharge limits: {}", Port.printBuffer(frame));
         return frame;
@@ -192,9 +192,9 @@ public class SolisHVInverterCANProcessor extends Inverter {
         final ByteBuffer frame = prepareSendFrame(0x00004240);
 
         // Maximum cell temperature (0.1C) offset -100C
-        frame.putShort((short) (pack.tempMax + 1000));
+        frame.putShort((short) (pack.tempMax - 100));
         // Minimum cell temperature (0.1C) offset -100C
-        frame.putShort((short) (pack.tempMin + 1000));
+        frame.putShort((short) (pack.tempMin - 100));
         // Maximum cell temperature cell number
         frame.putShort((short) pack.tempMaxCellNum);
         // Minimum cell temperature cell number
@@ -315,9 +315,9 @@ public class SolisHVInverterCANProcessor extends Inverter {
         final ByteBuffer frame = prepareSendFrame(0x00004270);
 
         // maximum module temperature (0.1C) offset -100C
-        frame.putShort((short) (pack.maxModuleTemp + 1000));
+        frame.putShort((short) (pack.maxModuleTemp - 100));
         // minimum module temperature (0.1C) offset -100C
-        frame.putShort((short) (pack.minModuleTemp + 1000));
+        frame.putShort((short) (pack.minModuleTemp - 100));
         // pack number with maximum module temperature
         frame.putShort((short) pack.maxModuleTempNum);
         // pack number with minimum module temperature
